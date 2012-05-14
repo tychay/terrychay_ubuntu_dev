953836b9701fa6f676c4f9753a83d0021b56a7b1
terry chay <tchay@wikimedia.org>
2012-05-13 18:09:06 -0700

    path symlinks
    Symlinks do not work in parallels file system, so I have to replace the symlinks with the actual files wherever possible :-(

--- bootstrap.sh
+++ bootstrap.sh
@@ -325,10 +325,14 @@ if [ ! -d $WORDPRESS_DIR ]; then
 	echo "### Syncing over wordpress htdocs..."
 	rsync -az -e "ssh -i ${SSH_KEY}" ${BITNAMI_ADDR}:${BITNAMI_WORDPRESS_HTDOCS} ${DEV_DIR}/wordpress
 fi
-# symlinks broken :-(
+# symlinks break in parallels filesystem :-(
 if [ ! -f $WORDPRESS_DIR/wordpress/htdocs/wp-content/sunrise.php ]; then
 	echo "### Fixing broken symlink (sunrise.php for domain mapping)..."
 	cp $WORDPRESS_DIR/wordpress/htdocs/wp-content/mu-plugins/wordpress-mu-domain-mapping/sunrise.php $WORDPRESS_DIR/wordpress/htdocs/wp-content/sunrise.php
+fi
+if [ ! -f $WORDPRESS_DIR/wordpress/htdocs/wp-content/mu-plugins/domain_mapping.php ]; then
+	echo "### Fixing broken symlink (domain_mapping.php for domain mapping)..."
+	cp $WORDPRESS_DIR/wordpress/htdocs/wp-content/mu-plugins/wordpress-mu-domain-mapping/domain_mapping.php $WORDPRESS_DIR/wordpress/htdocs/wp-content/mu-plugins/domain_mapping.php
 fi
 # }}}
 # set up ssh tunnel to mysqld {{{
